369a78bba7a74bb15fc7586c9d845f7b5669246b
ItzNecro <76115078+ItzNecro@users.noreply.github.com>
2021-02-27 14:50:52 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,6 +26,18 @@ client.on('message', message => {
 
 client.on('message', message => {
 
+    if (message.content === 'cih') {
+
+       message.reply('APA KAU CAH CIH CAH CIH!!!!);
+
+       }
+
+});
+
+
+
+client.on('message', message => {
+
     if (message.content === 'hai guys') {
 
        message.channel.send('lu yang gay dyno');
