c249f1c1ab4d103891271c4ded58fa8d95316d0f
ItzNecro <76115078+ItzNecro@users.noreply.github.com>
2021-03-03 12:12:16 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -165,8 +165,8 @@ client.on("message", (message) => {
   if (message.content.startsWith(prefix + "ping")) {
     message.channel.send("**pong!**");
   } else
-  if (message.content.startsWith(prefix + "foo")) {
-    message.channel.send("bar!");
+  if (message.content.startsWith(prefix + "gabut")) {
+    message.channel.send(":v");
   }
 });
 
