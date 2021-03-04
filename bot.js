ba317ce5b5f931312b78210b993e3f1628e0a99e
ItzNecro <76115078+ItzNecro@users.noreply.github.com>
2021-03-04 08:21:34 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -166,7 +166,35 @@ client.on("message", (message) => {
     message.channel.send("**pong!**");
   } else
   if (message.content.startsWith(prefix + "help")) {
-    message.channel.send("Sori coming soon niiiikk");
+    message.channel.send("{embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "This is an embed",
+    url: "http://google.com",
+    description: "This is a test embed to showcase what they look like and what they can do.",
+    fields: [{
+        name: "Fields",
+        value: "They can have different fields with small headlines."
+      },
+      {
+        name: "Masked links",
+        value: "You can put [masked links](http://google.com) inside of rich embeds."
+      },
+      {
+        name: "Markdown",
+        value: "You can put all the *usual* **__Markdown__** inside of them."
+      }
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: "© Example"
+    }
+  }
+});");
   }
 });
 
