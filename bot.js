01ad0cfc549c9f0d831b8fad0e6595d139acdabe
ItzNecro <76115078+ItzNecro@users.noreply.github.com>
2021-03-09 13:53:28 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -166,7 +166,7 @@ client.on("message", (message) => {
     message.channel.send("Hmm");
   } else
   if (message.content.startsWith(prefix + "help")) {
-    message.channel.send('Hmm');
+    message.channel.send(newEmbeds);
   }
  
 });
